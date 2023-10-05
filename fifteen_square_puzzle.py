08b512dc345703d11bc8a40cd03c4637d17fe6c0
unknown <olaniyigeorge77@gmail.com>
2023-10-05 21:37:17 +0100

    Rewriting using linkedlists

--- fifteen_square_puzzle.py
+++ fifteen_square_puzzle.py
@@ -136,63 +136,211 @@ class GameState():
 
         return (f'LEFT: Switched {zero_tile}({zero}), for {left_tile}({left})')
     
+# Refactor
+class GameCycle():
+
+    def __init__(self, data,  next=None, parent=None):
+        self.data = data
+        self.parent = parent
+        self.next = next
+
+class GameMoves():
+
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = GameCycle(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next_node:
+            current = current.next_node
+        current.next_node = new_node
+
+    def display(self):
+        pass
 
 
-# GAME PLAY
-def play():
+
+class Puzzle():
     print("                  Welcome              ")
     print("                    to                 ")
     print("           FIFTEEN SQUARE PUZZLE       ")
 
     print("Controls: \n To move up, press: U \n To move down, press: D \n To move left, press: L \n To move right, press: R")
+    print()
+    print()
 
-    moves = []
-    game_states = []
-
-    start_state = GameState()
-    game_states.append(start_state)
-
-    while not start_state.is_arranged():
-        current_state = start_state
-        current_state.show()
-
-        move = input('Make your move...')
-        moves.append(move)
-        if make_move(move, current_state) == None:
-                continue
-        
-        current_state.show
-        game_states.append(current_state)
-        
-
-
-    def make_move(move, state):
-        if move.lower('u'):
-            if not state.up():
-                return None
-              
-        elif move.lower('d'):
-            if not state.down():
-                return None
-        elif move.lower('l'):
-            if not state.left():
-                return None
-        elif move.lower('r'):
-            if not state.right():
-                return None
+    def __init__(self):
+        if not reversed:
+            self.goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
         else:
-            print("Invalid move")
-            return None
+            self.goal = [[0,15,14,13,],[12,11,10,9],[8,7,6,5],[4,3,2,1]]
+        
+        self.states = GameMoves()
+        self.start_config = None
+        self.solved = False
+
+    def shuffled_board(self):
+        '''
+        Generates a shuffled board relative to the goal
+        '''
+        board= []
+        tile_numbers= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        shuffled= False
+        while not shuffled:
+            for row in range(4):
+                xrow = []
+                for tile in range(4):
+                    choice= random.choice(tile_numbers)
+                    xrow.append(choice)
+                    tile_numbers.remove(choice)
+                board.append(xrow)
+            if board != self.goal:
+                shuffled = True
+        return board
+    
+
+    def play(self):
+        self.start_config = self.shuffled_board()
+        # self.states.append(start_config)
+
+        while not self.solved:
+            # Get the last state
+            current = self.get_last_state()
+            
+            # get_possible_valid_moves
+            # get_valid_input
+            # apply_move
+            # add result of a applying move and the valid_input to self.gamecycles {move: move, state: boardstate}
+            # if result of a applying move is arranged
+                # set self.solved to True
+            # Add make resultin boardstate a direct child of current
+            # display last state
+        
+            pass
+
+    def get_possible_valid_moves(self, boardstate):
+        '''
+        :params: boardstate -- list of lists with 4 x 4 config
+        :return: list
+        Takes a board state and returns a list of possible valid moves
+        ('U' | 'D' | 'L' | 'R) returns a list of all or some of these strings
+        '''
+        pass
+
+    def get_valid_input(self, valid_moves, input):
+        '''
+        :params: valid_moves -- list of strings
+        :params: input -- string
+        :return: string
+        Takes a list of valid moves(list of strings) and checks if input is in the list
+        If input not in list, keep prompting for valid input else return input'''
+        pass
+
+    def apply_move(self, move, boardstate):
+        '''
+        :params: move -- string
+        :params: boardstate -- list of lists with 4 x 4 config
+        :return: list of lists with 4 x 4 config
+        Takes a move and apply to the boardstate then returns a new boardstate
+        '''
+        pass
+
+    def get_empty(self, boardstate):
+        '''
+        :params: boardstate -- list of lists with 4 x 4 config
+        :return: tuple
+        Takes a boardstate then returns a tuple(coordinate of the position of 0 in the board)
+        '''
+        pass
+    
+    
+    # Game transition will be a (action, config) pair
+        ## ('u', [[...],[...],[...],[...]])
+
+    # u1-u2-u3-null4
+    # 1
+    def get_last_state(self):
+        '''
+        Takes the head of a linkedlist(start_config) and returns the youngest node(recent game cycle)
+        '''
+        current = self.start_config
+        while current.next_node:
+            current = current.next_node
+            if current.next_node == None:
+                return current
+        return current
+        
 
 
-    return None
-
-def train():
-    return None
-
-def play_with_AI():
-    return None
 
 
 
-play()
+
+
+
+
+
+
+
+# # GAME PLAY
+# def play():
+#     print("                  Welcome              ")
+#     print("                    to                 ")
+#     print("           FIFTEEN SQUARE PUZZLE       ")
+
+#     print("Controls: \n To move up, press: U \n To move down, press: D \n To move left, press: L \n To move right, press: R")
+
+#     moves = []
+#     game_states = []
+
+#     start_state = GameState()
+#     game_states.append(start_state)
+
+#     while not start_state.is_arranged():
+#         current_state = start_state
+#         current_state.show()
+
+#         move = input('Make your move...')
+#         moves.append(move)
+#         if make_move(move, current_state) == None:
+#                 continue
+        
+#         current_state.show
+#         game_states.append(current_state)
+        
+
+
+#     def make_move(move, state):
+#         if move.lower('u'):
+#             if not state.up():
+#                 return None
+              
+#         elif move.lower('d'):
+#             if not state.down():
+#                 return None
+#         elif move.lower('l'):
+#             if not state.left():
+#                 return None
+#         elif move.lower('r'):
+#             if not state.right():
+#                 return None
+#         else:
+#             print("Invalid move")
+#             return None
+
+
+#     return None
+
+# def train():
+#     return None
+
+# def play_with_AI():
+#     return None
+
+
+
+# play()
